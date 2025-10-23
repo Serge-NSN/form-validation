bbadc30b9cd0f7c54950dbb6d3f22e0116593ea9
Ntunyu Serge <ntunyuserge@gmail.com>
2025-10-23 18:54:51 +0000

    corrected

--- validation.js
+++ validation.js
@@ -1,23 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const email = document.getElementById('email');
-  const emailConfirm = document.getElementById('email-confirm');
-  const errorDiv = document.getElementById('email-error');
+function emailValidation() {
+  const form = document.getElementById('form');
+  const button = document.getElementsByClassName("submit_btn")[0]
+  let emailConfirmationRow = document.querySelector("#email_confirm").closest("tr");
+  let emailConfirm = document.querySelector("#email_confirm")
 
-  function validateEmails() {
-    if (emailConfirm.value === "") {
-      errorDiv.textContent = '';
-      emailConfirm.classList.remove('input-error');
-      return;
-    }
-    if (email.value !== emailConfirm.value) {
-      errorDiv.innerHTML = '<span class="error-message">Email does not match</span>';
-      emailConfirm.classList.add('input-error');
+  
+  
+  button.addEventListener('click', e => {
+    e.preventDefault();
+    if (form.email.value !== form.email_confirm.value) {
+      const element = document.createElement('p')
+      const message = document.createTextNode("Email does not match")
+      emailConfirmationRow.insertAdjacentElement("afterend", element);
+      element.appendChild(message);
+      element.classList.add("alert");
+      element.style.color = "#d14539";
+      emailConfirm.style.setProperty("background-color", "rgba(230,169,171,.5)", "important");
+      setTimeout(() => {
+        element.remove()
+      }, 3000)
     } else {
-      errorDiv.textContent = '';
-      emailConfirm.classList.remove('input-error');
+      form.submit();
     }
-  }
+  });
+};
 
-  email.addEventListener('input', validateEmails);
-  emailConfirm.addEventListener('input', validateEmails);
-});
+window.onload = emailValidation;
