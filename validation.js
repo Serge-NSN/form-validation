4a19cf769f2ab8adcbda3d9fcd2b172c117ae21e
Ntunyu Serge <ntunyuserge@gmail.com>
2025-10-23 13:50:44 +0000

    complete form validation

---
+++ validation.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const email = document.getElementById('email');
+  const emailConfirm = document.getElementById('email-confirm');
+  const errorDiv = document.getElementById('email-error');
+
+  function validateEmails() {
+    if (emailConfirm.value === "") {
+      errorDiv.textContent = '';
+      emailConfirm.classList.remove('input-error');
+      return;
+    }
+    if (email.value !== emailConfirm.value) {
+      errorDiv.innerHTML = '<span class="error-message">Emails do not match</span>';
+      emailConfirm.classList.add('input-error');
+    } else {
+      errorDiv.textContent = '';
+      emailConfirm.classList.remove('input-error');
+    }
+  }
+
+  email.addEventListener('input', validateEmails);
+  emailConfirm.addEventListener('input', validateEmails);
+});
